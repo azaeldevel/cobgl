41e6a38579725ad36e11f413caa69890d64eb15e
Azael Reyes (devel) <azael.devel@gmail.com>
2021-04-07 09:58:39 -0700

    Packing vertex

--- src/core.hh
+++ src/core.hh
@@ -7,17 +7,75 @@
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
-
-// Include GLEW
 #include <GL/glew.h>
-
-// Include GLFW
 #include <GLFW/glfw3.h>
-
-
+#include <vector>
+#include <octetos/core/Exception.hh>
 
 namespace octetos::cobgl
 {
+
+template<class T> struct Vertex
+{
+};
+template<class T> struct Vertex2D : public Vertex<T>
+{
+	T x;
+	T y;
+};
+template<class T> struct Vertex3D : public Vertex<T>
+{
+	T x;
+	T y;
+	T z;
+};
+template<class T> class VertexList : public std::vector<T>
+{
+public:
+	VertexList(unsigned short dimension, unsigned short size) : std::vector<T>(dimension * size)
+	{
+		if( size < 2 or size > 3)
+		{
+			std::string msg = "Solo se pueden crear objetos de 2 y 3 dimensiones, usted especifico '";
+			msg += std::to_string(size) + "'";
+			throw core::Exception(msg,__FILE__,__LINE__);
+		}
+		this->dimension = dimension;
+		this->size = size;
+	};
+	T* getHeader()
+	{
+		std::vector<T>& vl = *this;
+		return &vl[0];
+	}
+	Vertex<T>& operator[](unsigned short index)
+	{
+		std::vector<T>& vl = *this; 
+		if(dimension == 2) 
+		{
+			T* t = getHeader();
+			t = t + (sizeof(Vertex2D<T>) * index);
+			Vertex2D<T>* v = (Vertex2D<T>*) t ;
+			return *v;
+		}
+		else if(dimension == 3) 
+		{
+			T* t = getHeader();
+			t = t + ((sizeof(Vertex3D<T>)/sizeof(T)) * index);
+			Vertex3D<T>* v = (Vertex3D<T>*) t ;
+			return *v;
+		}
+		else
+		{
+			std::string msg = "Solo se pueden crear objetos de 2 y 3 domensiones, usted especifico '";
+			msg += std::to_string(size) + "'";
+			throw core::Exception(msg,__FILE__,__LINE__);
+		}
+	}
+private:
+	unsigned short dimension;
+	unsigned short size;
+};
 
 class Window
 {
--- src/main.cc
+++ src/main.cc
@@ -17,6 +17,10 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+#include <iostream>
 
 
 #include "core.hh"
@@ -37,9 +41,71 @@ int main( void )
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "src/SimpleVertexShader.vertexshader", "src/SimpleFragmentShader.fragmentshader" );
+	GLuint programID = LoadShaders( "src/SimpleTransform.vertexshader", "src/SingleColor.fragmentshader" );
 
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
+	// Projection matrix : Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+	
+	// Camera matrix
+	glm::mat4 View       = glm::lookAt(
+								glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+								glm::vec3(0,0,0), // and looks at the origin
+								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+						   );
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model      = glm::mat4(1.0f);
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix mul
+
+	
+	/*std::vector<std::vector<float>> vData(3);
+	vData[0].resize(3);
+	vData[0][0] = -1.0f;
+	vData[0][1] = -1.0f;
+	vData[0][2] = 0.0f;
+	vData[1].resize(3);
+	vData[1][0] = 1.0f;
+	vData[1][1] = -1.0f;
+	vData[1][2] = 0.0f;
+	vData[2].resize(3);
+	vData[2][0] = 0.0f;
+	vData[2][1] = 1.0f;
+	vData[2][2] = 0.0f;
+	std::cout << "Size of v1 " << vData[0].size() << "\n";
+	std::cout << "Size of v2 " << vData[1].size() << "\n";
+	std::cout << "Size of v3 " << vData[2].size() << "\n";
+	std::cout << "Size of vData " << vData.size() << "\n";*/
+	octetos::cobgl::VertexList<float> vecL(3,3);
+	octetos::cobgl::Vertex3D<float>& v1 = (octetos::cobgl::Vertex3D<float>&)vecL[0];
+	v1.x = -1.0;
+	v1.y = -1.0;
+	v1.z = 0.0;
+	octetos::cobgl::Vertex3D<float>& v2 = (octetos::cobgl::Vertex3D<float>&)vecL[1];
+	v2.x = 1.0;
+	v2.y = -1.0;
+	v2.z = 0.0;
+	octetos::cobgl::Vertex3D<float>& v3 = (octetos::cobgl::Vertex3D<float>&)vecL[2];
+	v3.x = 0.0;
+	v3.y = 1.0;
+	v3.z = 0.0;
+	std::cout << "Size of octetos::cobgl::Vertex3D<float> " << sizeof(octetos::cobgl::Vertex3D<float>) << "\n";
+	std::cout << "Size of float " << sizeof(float) << "\n";
+	/*std::vector<float> vec(9);
+	vec[0] = -1.0f;
+	vec[1] = -1.0f;
+	vec[2] = 0.0f;
+	vec[3] = 1.0f;
+	vec[4] = -1.0f;
+	vec[5] = 0.0f;
+	vec[6] = 0.0f;
+	vec[7] = 1.0f;
+	vec[8] = 0.0f;*/
+		
 	static const GLfloat g_vertex_buffer_data[] = { 
 		-1.0f, -1.0f, 0.0f,
 		 1.0f, -1.0f, 0.0f,
@@ -48,17 +114,22 @@ int main( void )
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * 9, vecL.getHeader(), GL_STATIC_DRAW);
 
 	
-	do{
+	do
+	{
 
 		// Clear the screen
 		glClear( GL_COLOR_BUFFER_BIT );
 
 		// Use our shader
 		glUseProgram(programID);
+
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
 		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
@@ -83,13 +154,12 @@ int main( void )
 		glfwPollEvents();
 
 	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(wnd, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(wnd) == 0 );
+	while( glfwGetKey(wnd, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(wnd) == 0 );
 	
-	// Cleanup VBO
+	// Cleanup VBO and shader
 	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteVertexArrays(1, &VertexArrayID);
 	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
 
 	return 0;
 }
